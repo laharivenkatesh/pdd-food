23f86aa9e6fe825d1b0bb3a717a476c763dfe6bd
saipradeepreddy1364 <bunny.akki21@gmail.com>
2026-08-07 17:01:13 +0530

    Update vite.config.ts to use process.cwd() in PDD-Frontend

--- PDD-Frontend/vite.config.ts
+++ PDD-Frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(process.cwd(), './src'),
       'react-native': 'react-native-web',
     },
     extensions: ['.web.tsx', '.web.ts', '.tsx', '.ts', '.web.js', '.js'],
